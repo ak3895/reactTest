c5d824091baed0ef15461b25786de1ba860764ee
Akshay3895 <akshay3895@gmail.com>
2019-11-14 15:00:58 +0530

    Updates

---
+++ precache-manifest.b131b01cd20c6dd6a7f8200249912c38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a235d5d88bf1c520c58c87c5794874",
+    "url": "/reactTest/index.html"
+  },
+  {
+    "revision": "dbfb37d114f93599e3f6",
+    "url": "/reactTest/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ff7b00600d233d974e30",
+    "url": "/reactTest/static/js/2.d20f16af.chunk.js"
+  },
+  {
+    "revision": "dbfb37d114f93599e3f6",
+    "url": "/reactTest/static/js/main.61e85913.chunk.js"
+  },
+  {
+    "revision": "f88895155061e1ed7dbe",
+    "url": "/reactTest/static/js/runtime-main.6fc71c2c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactTest/static/media/logo.25bf045c.svg"
+  }
+]);
